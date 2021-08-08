008a66a75b2afd2fdc67da3b602edf44cf177614
lukasz <lukasz0klepka@gmail.com>
2021-08-08 14:53:01 +0200

    Task v1.1 Solve problem for given unit tests Creating additional unit tests and eliminating emerging errors

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
-from tree import Node
+from tree import Node, LeafNode, InnerNode
+"""import the rest of classes"""
 
 class Code:
     """
@@ -27,5 +28,29 @@ class Code:
 
         All the auxiliary classes for building the tree are provided (see tree.py).
     """
+
+    """first solution"""
     def create_code(self, text: str) -> Node:
-        raise NotImplementedError
+        prepared_text = text.lower().replace(" ", "")
+        freq = {}
+        """make a dictionary with symbol and frequency"""
+        for symbol in prepared_text:
+            freq[symbol] = prepared_text.count(symbol)
+
+        """create the list that contains symbols and count of theirs occurrences"""
+        sorted_freq = {key: value for key, value in sorted(freq.items())}
+        nodes = [LeafNode(symbol, frequency) for symbol, frequency in sorted_freq.items()]
+        nodes.sort(key=lambda x: x.frequency, reverse=False)
+
+        while len(nodes) > 1:
+            """take two with lowest frequency, get rid of them too"""
+            node1 = nodes.pop(0)  # [0].copy()
+            node2 = nodes.pop(0)  # .copy()
+            """merge that"""
+            new_node = InnerNode(node1, node2)
+            """joined node goes back"""
+            nodes.append(new_node)
+            """get them sorted again"""
+            nodes.sort(key=lambda x: x.frequency, reverse=False)
+
+        return nodes[0]
--- code_test.py
+++ code_test.py
@@ -46,3 +46,73 @@ class CodeTest(unittest.TestCase):
                              LeafNode("a", 13)
                          )
                          )
+
+    def test_short(self):
+        code = Code()
+        self.assertEqual(code.create_code("d"),
+                         LeafNode("d", 1))
+
+    def test_deep_tree_different_start(self):
+        code = Code()
+        result = code.create_code("deeaaaaaaaaaabbcdddeeeaaaaa")
+        self.assertEqual(result,
+                         InnerNode(
+                             InnerNode(
+                                 LeafNode("e", 5),
+                                 InnerNode(
+                                     InnerNode(
+                                         LeafNode("c", 1),
+                                        LeafNode("b", 2)
+                                        ),
+                                     LeafNode("d", 4)
+                                 )
+                             ),
+                             LeafNode("a", 15)
+                         )
+                         )
+
+    def test_equal_frequency(self):
+        """this test showed a method error, when different symbols had the same number of repetitions,
+         the solution did not sort them in alphabetical order"""
+        code = Code()
+        result = code.create_code("dacb")
+        self.assertEqual(result,
+                         InnerNode(
+                             InnerNode(
+                                 LeafNode("a", 1),
+                                 LeafNode("b", 1)
+                             ),
+                             InnerNode(
+                                 LeafNode("c", 1),
+                                 LeafNode("d", 1)
+                             )
+                         )
+                         )
+
+    def test_upper_and_lower(self):
+        """this test showed a method error, when the starting text has different symbol sizes"""
+        code = Code()
+        result = code.create_code("aaAbcaB")
+        self.assertEqual(result,
+                         InnerNode(
+                             InnerNode(
+                                 LeafNode("c", 1),
+                                 LeafNode("b", 2)
+                             ),
+                             LeafNode("a", 4)
+                         )
+                         )
+
+    def test_with_spaces(self):
+        """this test showed a method error, when the starting text has spaces"""
+        code = Code()
+        result = code.create_code("aa bc aba")
+        self.assertEqual(result,
+                         InnerNode(
+                             InnerNode(
+                                 LeafNode("c", 1),
+                                 LeafNode("b", 2)
+                             ),
+                             LeafNode("a", 4)
+                         )
+                         )
